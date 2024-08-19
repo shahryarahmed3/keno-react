b75e68c3dba058c0a26c76c3ceb52e352828c3a7
Shahryar Ahmed <sahme34@uic.edu>
2024-08-19 00:29:11 -0500

    updated app

---
+++ keno-react/src/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.css"
+
+
+function Header() {
+    return (
+        <div class="logo-container">
+            <section class="stage">
+                <figure class="ball ball1">
+                    K
+                    <span class="shadow"></span>
+                </figure>
+                <figure class="ball ball2">
+                    E
+                    <span class="shadow"></span>
+                </figure>
+                <figure class="ball ball3">
+                    N
+                    <span class="shadow"></span>
+                </figure>
+                <figure class="ball ball4">
+                    O
+                    <span class="shadow"></span>
+                </figure>
+            </section>
+        </div>
+    );
+}
+
+export default Header;
